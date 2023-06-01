cf24b8fa50a7a7bf0e29a88facbee080824e813c
Morakinyo Joseph <97817101+Morakinyo-Joseph@users.noreply.github.com>
2023-06-01 11:40:00 +0100

    Update settings.py

--- panel/settings.py
+++ panel/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yhlx7vtj!_h@u0&sn99)6)%t8-#&^7+p5bvq(u0^g#t2q0(g#3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
